11ff5cae3e7a971a332a75c8f64f255022e69f96
Ahmad Firdaus <37030208+Panda5200@users.noreply.github.com>
2020-09-22 20:29:28 +0800

    Add files via upload

---
+++ admin/importexport.php
@@ -0,0 +1,87 @@
+<?php
+include('../config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD List Apps</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">CRUD List Apps</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collappse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="create.php">Tambah</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="importexport.php">Import/Export</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <form class="form-inline my-2 my-lg-0">
+                        <a href="logout.php" class="btn btn-danger" role="button">Logout</a>
+                    </form>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container" style="margin-top:20px">
+        <h2>Import/Export Data</h2>
+
+        <hr>
+
+        <div class="row">
+            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                <fieldset>
+                    <!-- Form Name -->
+                    <legend>Select File</legend>
+                    <!-- File Button -->
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <input type="file" name="file" id="file" class="input-large">
+                        </div>
+                    </div>
+                    <!-- Button -->
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+
+        <div>
+            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                <div class="form-group">
+                    <div class="row justify-content-end">
+                        <div class="col-3">
+                            <input type="submit" name="Export" class="btn btn-success" value="Export to CSV"/>
+                        </div>
+                    </div>
+                </div>                    
+            </form>           
+        </div>
+    
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
